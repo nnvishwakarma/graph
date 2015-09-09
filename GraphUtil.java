b6d60b8bd52a238c051ab32cee11e81f893b53f3
nnvishwakarma <nnvishwakarma@gmail.com>
2015-09-09 18:48:40 +0530

    Create GraphUtil.java

---
+++ GraphUtil.java
@@ -0,0 +1,56 @@
+package org.fware.beans;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class GraphUtil {/*	
+	
+	
+	
+	public void addVertex(Vertex vertex,Graph graph)	{
+		graph.getVertices().add(vertex);
+	}
+	public void removeVertex(Vertex vertex,Graph graph) {
+		
+	}
+	
+	public void addEdge(Edge edge,Graph graph)	{
+		graph.getEdges().add(edge);
+	}
+	public void removeEdge(Edge edge,Graph graph) {
+		
+	}
+	
+	public void setGraphMatrix(int[][] graphMatrix) {
+		this.graphMatrix = graphMatrix;
+	}
+
+	
+
+	public void setNoOfVertices(Integer noOfVertices) {
+		this.noOfVertices = noOfVertices;
+	}
+
+	
+	private void setStrVertices(String[] strVertices) {
+		this.strVertices = strVertices;
+	}
+
+		
+	
+	
+
+public void prepareGraphMatix(Graph graph){
+		
+		graph.setStrVertices(graph.getVertices());
+		setVerticesIndex(getVertices());
+		
+		this.graphMatrix=new int[getNoOfVertices()][getNoOfVertices()];
+		Initializing Label of Graph Matrix 		
+		for(Edge cEdge : getEdges()){
+			graphMatrix[FromVertexIndex][ToVertexIndex]=1;
+			graphMatrix[getVerticesIndex().get(cEdge.getFromVertex())][getVerticesIndex().get(cEdge.getToVertex())]=1;
+		}
+	}
+*/}
